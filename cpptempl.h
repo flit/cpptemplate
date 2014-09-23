5220e430a056c40d5f663e3a234635af8e5a1d9b
Chris Reed <flit@me.com>
2014-09-23 16:19:13 -0500

    Updated documentation.
    - Described subtemplates and comment statements.
    
    Signed-off-by: Chris Reed <flit@me.com>

--- cpptempl.h
+++ cpptempl.h
@@ -31,7 +31,8 @@ There are two main functions that users will interact with, both called ``parse(
 are in the ``cpptempl`` namespace. The prototypes for these functions are as follows::
 
     std::string parse(std::string templ_text, data_map &data);
-    void parse(std::ostream &stream, std::string templ_text, data_map &data) ;
+
+    void parse(std::ostream &stream, std::string templ_text, data_map &data);
 
 Both take the template text as a ``std::string`` along with a data_map that has the variable
 values to substitute. One form of ``parse()`` simply returns the result of the template
@@ -50,7 +51,11 @@ Syntax
 :If:
     ``{% if person.name == "Bob" %}Full name: Robert{% endif %}``
 :Def:
-    ``{% def foobar %}template to store in the named variable{% enddef %}``
+    ``{% def foobar %}template to store in the {$named} variable{% enddef %}``
+:Comment:
+    ``{# comment goes here #}``
+
+Control statements on a line by themselves will eat the newline following the statement.
 
 Anywhere a variable name is used, you may use a dotted key path to walk through the
 keys of nested data_map objects. The elements of the key path used with a def statement
@@ -77,9 +82,7 @@ Inside a for statement block, a "loop" map variable is defined with these keys:
 ``count``   Total number of elements in the list
 ==========  =======================================================
 
-Def statements evaluate the template between the def and enddef and store the results
-in the named variable, which may be a path. The def template contents are evaluated
-only once at the point of the def statement, not when the def variable is used.
+Def statements are described below under the Subtemplates section.
 
 There are a few pseudo-functions that may be used in if statements and variable
 substitution. More may be added later.
@@ -90,14 +93,18 @@ substitution. More may be added later.
 ``empty(x)``     True if the variable path x is the empty string.
 ===============  ===========================================================
 
-Control statements inside ``{% %}`` may be commented at the end of the statement. Comments
-are started with ``--`` and run to the close brace of the statement::
+Control statements inside ``{% %}`` brackets may be commented at the end of the statement.
+This style of comment is started with ``--`` and runs to the close bracket of the statement,
+as demonstrated here::
 
     {% for person in people %}
-    Name: {$person.name
+    Name: {$person.name}
     {% endfor -- end the person loop %}
 
-Control statements on a line by themselves will eat the newline following the statement.
+You may also put comments in ``{# #}`` brackets. These comments may span multiple lines.
+They will not be copied to the output under any circumstances. And, as with all control
+statements, if such a comment is on a line by itself, the newline following the comment
+is absorbed and not reproduced in the output.
 
 Types
 ==================
@@ -109,9 +116,31 @@ These are the built-in data types::
     std::string
     data_list
     data_map
+    subtemplate
 
 All other types are converted to strings using ``boost::lexical_cast`` when set in
 a data_ptr or data_map.
+
+Bool values will result in either "true" or "false" when substituted. data_list or
+data_map values will cause a TemplateException to the thrown if you attempt to
+substitute them as a variable.
+
+Subtemplates
+==================
+Subtemplates are a special type. They allow you to define a template once and reuse
+it multiple times by substituting it as a variable. A subtemplate is completely
+re-evaluated every time it is substituted, using the current values of any variables.
+This is particularly useful within a loop.
+
+There are two ways to define a subtemplate. The first is to use the ``make_template()``
+function. It takes a std::string and returns a subtemplate data_ptr, which may then
+be stored in a data_map.
+
+The second way to create a subtemplate is to use the def statement within a template.
+Def statements define a subtemplate with the template contents between the def and
+enddef statements. The subtemplate is stored in the named variable, which may be a path.
+As with all subtemplates, the contents are evaluated at the point where the def variable
+is used.
 
 Handy Functions
 ========================
@@ -132,6 +161,8 @@ data_map elements::
     person["age"] = 35;
     person["name"] = "Fred";
     person["has_pet"] = true;
+
+``make_template()`` : Creates a subtemplate from a std::string.
 
 Errors
 ==================
